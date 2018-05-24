1bac1f8440b4389d79a0cec6d51fb36b3ecd9a77
Hamtaro <damien@guillot.nu>
2018-05-24 15:56:14 +0200

    feat: add sound playing (#15)
    * Play sounds (list on the app Mi Home)
    
    Just use gateway.setSound(number)
    
    numbers :
          - "0 - Police car 1"
          - "1 - Police car 2"
          - "2 - Accident"
          - "3 - Countdown"
          - "4 - Ghost"
          - "5 - Sniper rifle"
          - "6 - Battle"
          - "7 - Air raid"
          - "8 - Bark"
          - "10 - Doorbell"
          - "11 - Knock at a door"
          - "12 - Amuse"
          - "13 - Alarm clock"
          - "20 - MiMix"
          - "21 - Enthusiastic"
          - "22 - GuitarClassic"
          - "23 - IceWorldPiano"
          - "24 - LeisureTime"
          - "25 - ChildHood"
          - "26 - MorningStreamLiet"
          - "27 - MusicBox"
          - "28 - Orange"
          - "29 - Thinker"
    
    And custom sounds recorded on the app starts with id 10001
    
    * Volume
    
    Use : setSound(ID sound, volume)
    
    * Add gateway.setSound usage
    
    - "0 - Police car 1"
    - "1 - Police car 2"
    - "2 - Accident"
    - "3 - Countdown"
    - "4 - Ghost"
    - "5 - Sniper rifle"
    - "6 - Battle"
    - "7 - Air raid"
    - "8 - Bark"
    - "10 - Doorbell"
    - "11 - Knock at a door"
    - "12 - Amuse"
    - "13 - Alarm clock"
    - "20 - MiMix"
    - "21 - Enthusiastic"
    - "22 - GuitarClassic"
    - "23 - IceWorldPiano"
    - "24 - LeisureTime"
    - "25 - ChildHood"
    - "26 - MorningStreamLiet"
    - "27 - MusicBox"
    - "28 - Orange"
    - "29 - Thinker"
    
    And custom sounds recorded on the app starts with id 10001

--- src/lib/gateway.js
+++ src/lib/gateway.js
@@ -23,6 +23,9 @@ class Gateway extends events.EventEmitter {
 
     this._color = { r: 0, g: 0, b: 0 }
 
+    this._sound = 10000
+    this._volume = 0
+
     this._subdevices = new Map()
 
     const payload = '{"cmd": "get_id_list"}'
@@ -157,6 +160,12 @@ class Gateway extends events.EventEmitter {
     this._sendUnicast(payload)
   }
 
+  _writeSound () {
+
+    const payload = `{"cmd": "write", "model": "gateway", "sid": "${this._sid}", "short_id": 0, "data": "{\\"mid\\":${this._sound}, \\"vol\\":${this._volume}, \\"key\\": \\"${this._key}\\"}"}`
+    this._sendUnicast(payload)
+  }
+
   get ip () { return this._ip }
   get sid () { return this._sid }
   get ready () { return this._ready }
@@ -173,6 +182,16 @@ class Gateway extends events.EventEmitter {
 
     this._color = color
     this._writeColor()
+  }
+
+  get sound () { return this._sound }
+  get volume () { return this._volume }
+  setSound (sound, volume) {
+    if (!this._ready) return
+
+    this._sound = sound
+    this._volume = volume
+    this._writeSound()
   }
 
   get intensity () { return this._intensity }
